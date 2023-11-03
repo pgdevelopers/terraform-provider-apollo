037aeb3cb3518b169ef022063133f01c9fa4abf6
chaddymac <moodychadiamond@yahoo.com>
2023-11-03 10:19:01 -0400

    Stop tracking tf files

--- examples/nate-cool-examples/outputs.tf
+++
@@ -1,9 +0,0 @@
-# output "graph_id" {
-#   description = "ID of apollo graph"
-#   value       = apollo_graph.graph.graph_id
-# }
-
-# output "graph_key" {
-#   description = "API Key of apollo graph"
-#   value       = apollo_apikey.apikey.token
-# }
